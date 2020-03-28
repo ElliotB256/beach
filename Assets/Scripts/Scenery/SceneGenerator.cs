1ac3ec9e141543d287a67129215e7b795017cb7f
Elliot Bentine <ebentine@bitbucket.org>
2020-03-28 13:18:42 +0000

    Added some scenery sprites, tweaked the bin colors

---
+++ Assets/Scripts/Scenery/SceneGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Entities;
+
+namespace Beach.Scenery
+{
+    [GenerateAuthoringComponent]
+    [Serializable]
+    public struct SceneGenerator : IComponentData
+    {
+        public int Width;
+        public int Height;
+    }
+}
