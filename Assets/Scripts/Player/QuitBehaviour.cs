258ebf0f0c29780bef046acd70624a47961c3d4f
Elliot Bentine <ebentine@bitbucket.org>
2020-03-29 21:38:56 +0100

    Added quit behaviour

---
+++ Assets/Scripts/Player/QuitBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Beach.Player
+{
+    public class QuitBehaviour : MonoBehaviour
+    {
+        protected void Update()
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
