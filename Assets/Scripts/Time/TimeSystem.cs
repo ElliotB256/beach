f625773bb6eaf1be6b99d38ccb628d3c9d5690ed
Pobiega <robin.aakerman@gmail.com>
2020-03-29 20:44:20 +0200

    Add a basic time system

---
+++ Assets/Scripts/Time/TimeSystem.cs
@@ -0,0 +1,18 @@
+﻿using Beach.Digging;
+using Beach.Carry;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Beach.Time
+{
+    public class TimeSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            Entities.ForEach((ref TimeHolder timeHolder) =>
+            {
+                timeHolder.TimeRemaining -= Time.DeltaTime;
+            });
+        }
+    }
+}
